c6e89c024636f405f5a7211b088c7fdcb2f352f2
AmeyaGurjar <amey992004@gmail.com>
2023-07-15 18:30:32 +0530

    Meow

--- main.py
+++ main.py
@@ -10,9 +10,9 @@ def jsonloader(jsonFile="data.json"):
     with open(jsonFile, "r") as jsonFile:
         return load(jsonFile)
 
-@TgBot.message_handler(commands=jsonloader()["commands"])
+@TgBot.message_handler(commands=jsonloader()["func"].keys())
 def command_handler(msg):
-    for command in jsonloader()["commands"]:
+    for command in jsonloader()["func"].keys():
         if (msg.text==command):
             try:
                 TgBot.send_message(msg, jsonloader()["func"][msg.text])
